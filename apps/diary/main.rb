44e4210b812dd2bb1bb1b32526205ff114177d5f
Ruby45 <discuss.ruby@gmail.com>
2023-04-15 22:10:49 +0300

    add write in file

--- apps/diary/main.rb
+++ apps/diary/main.rb
@@ -12,3 +12,11 @@ time = Time.now
 
 file_name = time.strftime('%Y-%m-%d')
 time_string = time.strftime('%H:%M')
+
+separator = "________________________________"
+
+file = File.new(current_puth + "/" + file_name + ".txt", 'a')
+
+file.print("\n\r" + time_string + "\n\r")
+
+all_lines.pop
