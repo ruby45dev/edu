271eaef4bc2c3b14c9a34361f90559ff767665b3
Ruby45 <discuss.ruby@gmail.com>
2023-04-18 12:29:08 +0300

    add new main.rb

--- apps/files1/main.rb
+++ apps/files1/main.rb
@@ -1,20 +1,8 @@
-# if File.exist?("./data/data1.txt")
-
-
-#     readfiles1 = File.new("./data/data.txt", "r")
-
-#     content = readfiles1.read
-
-#     puts content
-# else 
-#     puts "Файл удален или его не существует"
-# end
-
-file_path = "./data/data.txt"
-
-if File.exist?(file_path)
-    file_open = File.new(file_path)
-    puts content = file_open.read
+if File.exist?("./data/data.txt")
+    file = File.new("./data/data.txt")
+    content = file.readlines
+    file.close
+    puts content.sample
 else
-    puts "Файла нет"
+    "Файл не знайдено"
 end
--- apps/films/lib/film.rb
+++ apps/films/lib/film.rb
@@ -1,3 +1,21 @@
 class Film
 
+    attr_reader :title, :editor, :year
+
+    def initalize(title, editor, year)
+        @title = title
+        @editor = editor
+        @year = year
+    end
+
+    def file_read
+        f = Dir["./data/*.txt" ]
+        f.map do |file|
+            f2 = file.readlines
+        end
+
+    end
+
+
+
 end
