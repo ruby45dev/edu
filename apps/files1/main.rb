a9dbe1b5b5e518fd62d8f7a18e786c0ddee884ed
Ruby45 <discuss.ruby@gmail.com>
2023-05-02 16:39:40 +0300

    rename variable file => myfile

--- apps/files1/main.rb
+++ apps/files1/main.rb
@@ -12,9 +12,9 @@
 path_file = File.join(__dir__, "data", "data.txt")
 
 if File.exist?(path_file)
-    file = File.new(path_file)
-    content = file.readlines
-    file.close
+    myfile = File.new(path_file)
+    content = myfile.readlines
+    myfile.close
     puts content.sample
 else
     "Файл не знайдено"
