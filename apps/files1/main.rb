92365b093a2ccc23ce9f4bf30281e08a657eec4a
Ruby45 <discuss.ruby@gmail.com>
2023-05-02 16:37:59 +0300

    add File.join_

--- apps/files1/main.rb
+++ apps/files1/main.rb
@@ -1,4 +1,15 @@
-path_file = "#{__dir__}/data/data.txt"
+# path_file = "#{__dir__}/data/data.txt"
+
+# if File.exist?(path_file)
+#     file = File.new(path_file)
+#     content = file.readlines
+#     file.close
+#     puts content.sample
+# else
+#     "Файл не знайдено"
+# end
+
+path_file = File.join(__dir__, "data", "data.txt")
 
 if File.exist?(path_file)
     file = File.new(path_file)
