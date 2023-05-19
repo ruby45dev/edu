78dced41f604d09fc1cfdfb052ef9e205c40baaa
Ruby45 <discuss.ruby@gmail.com>
2023-05-19 17:22:43 +0300

    refactoring if

--- apps/list_files/main.rb
+++ apps/list_files/main.rb
@@ -1,9 +1,9 @@
-puth = "./data/*.txt"
+path = "./data/*.txt"
 
-files = Dir[puth]
+files_path = Dir[path]
 
-files.each_with_index do |file, index|
-    puts "#{index + 1}: #{file}"
+files_path.each_with_index do |file_name, index|
+    puts "#{index}: #{file_name}"
 
 end
 
@@ -11,28 +11,7 @@ puts "Вибирайте файл для перегляду"
 
 user_file = gets.to_i
 
-if user_file == 1
-    file1 = File.new("./data/file1.txt")
-    content_file1 = file1.read
-    puts "Це у файлі 1:"
-    puts content_file1
-    file1.close
-elsif user_file == 2
-    file2 = File.new("./data/file2.txt")
-    content_file2 = file2.read
-    puts "Це у файлі 2:"
-    puts content_file2
-    file2.close
-elsif user_file == 3
-    file3 = File.new("./data/file3.txt")
-    content_file3 = file3.read
-    puts "Це у файлі 3:"
-    puts content_file3
-    file3.close
-elsif user_file == 4
-    file4 = File.new("./data/file4.txt")
-    content_file4 = file4.read
-    puts "Це у файлі 4:"
-    puts content_file4
-    file4.close
-end
+files = files_path[user_file]
+    File.open(files) do |item|
+        puts item.read
+    end
