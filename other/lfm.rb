0d817d14959703a3b0cb6b1ba1d5041ba8430581
Ruby45 <discuss.ruby@gmail.com>
2023-06-02 07:48:50 +0300

    add blank lfm.rb

---
+++ other/lfm.rb
@@ -0,0 +1,10 @@
+def name(last_name, first_name, middle_name)
+    
+end
+
+puts 'Введіть прізвище: '
+last_name = gets.chomp
+puts 'Введіть імя: '
+first_name = gets.chomp
+puts 'Введіть по батькові: '
+middle_name = gets.chomp
