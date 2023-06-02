76ce22debdd706f01befc50b98cbd5369102811c
Ruby45 <discuss.ruby@gmail.com>
2023-06-02 17:00:21 +0300

    the end?

--- other/lfm.rb
+++ other/lfm.rb
@@ -1,5 +1,5 @@
-def name(last_name, first_name, middle_name)
-    
+def name(fname, mname)
+    fname[0] + "." + mname[0] + "."
 end
 
 puts 'Введіть прізвище: '
@@ -8,3 +8,7 @@ puts 'Введіть імя: '
 first_name = gets.chomp
 puts 'Введіть по батькові: '
 middle_name = gets.chomp
+
+result = last_name + " " + name(first_name, middle_name)
+
+puts "Ваше ім'я #{result}"
