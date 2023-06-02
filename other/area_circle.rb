a0a0e29fd6f663c4e88a48e10a9987fc98d8dd64
Ruby45 <discuss.ruby@gmail.com>
2023-06-02 07:05:40 +0300

    end

--- other/area_circle.rb
+++ other/area_circle.rb
@@ -1,8 +1,11 @@
-puts 'Введіть радіус кола: '
-radius_circle = gets.to_i
+# s = π × r2
 
 def area(radius_circle)
-
+    PI * (radius_circle ** 2)
 end
 
-puts "Площа кола складає: #{}"
+puts 'Введіть радіус кола: '
+radius_circle = gets.to_i
+PI = 3.14
+
+puts "Площа кола складає: #{area(radius_circle)}"
