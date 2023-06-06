b8789881a955f03e7600576ec1731968d2b986e5
Ruby45 <discuss.ruby@gmail.com>
2023-06-06 10:45:58 +0300

    add new file

--- other/rsp.rb
+++ other/rsp.rb
@@ -1,9 +1,2 @@
 # game "rock, scissors, paper"
 
-variables = ["камінь", "ножиці", "папір"]
-variables.each_with_index do |variable, index|
-    puts "#{index}. #{variable}"
-end
-
-pust 'Введіть число: '
-user_input = gets.to_i
