acdf90822b96fa1955415d931a1e11dc7d13b215
Ruby45 <discuss.ruby@gmail.com>
2023-06-06 15:06:16 +0300

    добавлено хто та що вибрав

--- other/rsp.rb
+++ other/rsp.rb
@@ -1,2 +1,16 @@
 # game "rock, scissors, paper"
+puts 'Зробіть свій вибір: '
 
+options = ['камінь', 'ножиці', 'бумага']
+
+options.each_with_index do |option, index|
+    puts "#{index}. #{option}"
+end
+
+puts 'Введіть відповідне число: '
+user_input = gets.to_i
+
+pc_input = rand(options.size)
+
+puts "Ви вибрали: #{user_input}"
+puts "ПК вибрав: #{pc_input}"
