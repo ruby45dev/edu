da1c5953a609b0699e1c8933a72bce415b739e85
Ruby45 <discuss.ruby@gmail.com>
2023-06-06 15:32:43 +0300

    додав хто та що вибрав у текстовому форматі

--- other/rsp.rb
+++ other/rsp.rb
@@ -1,4 +1,5 @@
 # game "rock, scissors, paper"
+
 puts 'Зробіть свій вибір: '
 
 options = ['камінь', 'ножиці', 'бумага']
@@ -10,13 +11,16 @@ end
 puts 'Введіть відповідне число: '
 user_input = gets.to_i
 
+user_text = options[user_input]
+puts "Ви вибрали: #{user_text}"
+
 pc_input = rand(options.size)
 
-puts "Ви вибрали: #{user_input}"
-puts "ПК вибрав: #{pc_input}"
+pc_text = options[pc_input]
+puts "ПК вибрав: #{pc_text}"
 
 if user_input == pc_input
-    puts 'нічия'
+    puts 'Нічия'
 elsif (user_input == 0 && pc_input == 1) ||
     (user_input == 1 && pc_input == 2) 
     puts 'Ви виграли'
