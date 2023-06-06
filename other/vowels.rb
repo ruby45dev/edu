b5532c4491905bc25fedb6888f54f2f8b68e1394
Ruby45 <discuss.ruby@gmail.com>
2023-06-06 19:59:45 +0300

    додав масиви

--- other/vowels.rb
+++ other/vowels.rb
@@ -1,8 +1,12 @@
-
 def count_vowels(word)
+    letters_vowels = %w(й е ї і а о є я и ю)
+    letters_user = word.chars
 end
 
-puts 'Ласкаов прошу до сервісу, щло рахує голосні!'
+puts 'Ласкаво прошу до сервісу, що рахує голосні!'
 sleep 1 
 puts 'Введіть слово: '
 user_word = gets.chomp
+
+puts "Голосних у слові #{user_word} - #{count_vowels(user_word)}"
+
