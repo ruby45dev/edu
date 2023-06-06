cd7a8799fc9291f0ae2706bc37919e6169e46875
Ruby45 <discuss.ruby@gmail.com>
2023-06-06 20:04:30 +0300

    додав each, та те що повертає метод. Завершив.

--- other/vowels.rb
+++ other/vowels.rb
@@ -1,6 +1,16 @@
 def count_vowels(word)
     letters_vowels = %w(й е ї і а о є я и ю)
     letters_user = word.chars
+
+    count = 0
+
+    letters_user.each do |letter_user|
+        if letters_vowels.include?(letter_user)
+            count += 1
+        end
+    end
+
+    count
 end
 
 puts 'Ласкаво прошу до сервісу, що рахує голосні!'
