c37dfadae42c0cb5d0b475fc020162b100179977
Ruby45 <discuss.ruby@gmail.com>
2023-06-19 11:29:44 +0300

    add files data and add balls

--- apps/qa_pl_ua/main.rb
+++ apps/qa_pl_ua/main.rb
@@ -6,12 +6,13 @@ quiz_data =
     file_path.map {|file_name| File.readlines(file_name, chomp: true)}
 
 count = 0
-QUIZ_SAMPLE_QA = 3
+balls = 0
+QUIZ_SAMPLE_QA = 4
 quiz_data.sample(QUIZ_SAMPLE_QA).each do |questions_and_answer|
     # puts questions = questions_and_answer[0]
     # answer = questions_and_answer[1]
 
-    questions, answer = questions_and_answer
+    questions, answer, ball = questions_and_answer
 
     puts questions
 
@@ -20,9 +21,12 @@ quiz_data.sample(QUIZ_SAMPLE_QA).each do |questions_and_answer|
     if user_answer == answer
         puts 'Красавєлло!'
         count += 1
+        balls += ball.to_i
     else
         puts "Не угадал! Правильній ответ: #{answer}"
     end
 end
 
 puts "Дано правильніх ответов #{count} из #{quiz_data.size}"
+
+puts "Вы набрали #{balls} баллів"
