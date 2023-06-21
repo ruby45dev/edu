230ea485716daaab4000858c1c92aaa9419b8a22
Ruby45 <discuss.ruby@gmail.com>
2023-06-21 11:23:47 +0300

    add method full_name

--- apps/adress_book/lib/person.rb
+++ apps/adress_book/lib/person.rb
@@ -1,8 +1,12 @@
-class Person
+class Persosn
     attr_reader :first_name, :last_name, :email_name
     def initialize(first_name, last_name, email_name)
       @first_name = first_name
       @last_name = last_name
       @email_name = email_name
+    end
+
+    def full_name
+      "#{@first_nsame} #{@last_name} #{@email_name}"
     end
 end
