748ba9144da86a99a03a50c174a462de51360ee9
Ruby45 <discuss.ruby@gmail.com>
2023-06-21 11:59:45 +0300

    add attr_reader, initalize

---
+++ apps/credit/lib/credit_request.rb
@@ -0,0 +1,13 @@
+class CreditRequest
+    
+    attr_reader :age, :floor, :salary, :credit_history, :amount_credit
+    
+    def initilize(age, floor, salary, credit_history, amount_credit)
+        @age = agess
+        @floor = floor
+        @salary = salary
+        @credit_history = credit_history
+        @amount_credit = amount_credit        
+    end
+end
+
