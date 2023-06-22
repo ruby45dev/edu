6f6d4b17b0630a4618d3c3b0992e13ce0987a969
Ruby45 <discuss.ruby@gmail.com>
2023-06-22 07:30:24 +0300

    add logic method scoring_ball

--- apps/credit/lib/credit_request.rb
+++ apps/credit/lib/credit_request.rb
@@ -2,12 +2,45 @@ class CreditRequest
     
     attr_reader :age, :floor, :salary, :credit_history, :amount_credit
     
-    def initilize(age, floor, salary, credit_history, amount_credit)
-        @age = agess
+    def initialize(age, floor, salary, credit_history, amount_credit)
+        @age = age
         @floor = floor
         @salary = salary
         @credit_history = credit_history
-        @amount_credit = amount_credit        
+        @amount_credit = amount_credit
+    end
+
+    
+
+    def scoring_ball     
+        result = 0   
+        
+        if @age >= 21 && @age <= 40
+            result += 10
+        elsif @age > 40
+            result += 20
+        end
+
+        if @floor == "w"
+            result += 10
+        end
+
+        if @salary >= 20001 && @salary <= 40000
+            result += 10
+        elsif @salary > 40000
+            result += 20
+        end
+        
+        if @credit_history == "y"
+            result += 20
+        end
+
+        if @amount_credit < 20000
+            result += 20
+        elsif @amount_credit >= 20001 && @amount_credit <= 40000
+            result += 10
+        end   
+
+        return result        
     end
 end
-
