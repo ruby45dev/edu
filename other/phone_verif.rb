8b528a24652851e8dd50a324f77c4030b64ab154
Ruby45 <discuss.ruby@gmail.com>
2023-07-20 12:46:48 +0300

    додав цикл, regex доопрацювати

--- other/phone_verif.rb
+++ other/phone_verif.rb
@@ -1,4 +1,15 @@
-puts 'Веддіть ваш номер телефону для перевірки: '
-user_phone = gets.strip
+loop do
 
-user_phone.match(phone_temp)
+    puts 'Веддіть ваш номер телефону для перевірки: '
+    user_phone = gets.strip
+
+    phone_temp = /^[+\0-9\-\ \()]+/
+
+    if user_phone.match(phone_temp)
+        puts 'Дякую! Номер перевірено'
+        break
+    else 
+        puts 'Введіть корректний номер телефону'
+    end
+
+end
