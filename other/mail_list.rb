5aac40681fd8624ff512a8f2168bb75623fbab33
Ruby45 <discuss.ruby@gmail.com>
2023-07-20 11:57:42 +0300

    add loop

--- other/mail_list.rb
+++ other/mail_list.rb
@@ -1,10 +1,14 @@
-puts 'Введіть email для перевірки'
-email_user = gets.strip 
+loop do
+    puts 'Введіть email для перевірки'
+    email_user = gets.strip 
 
-email_templ = /^[0-9a-z\.]+@[0-9a-z\.]+\.[a-z]+/
+    email_templ = /^[0-9a-z\.]+@[0-9a-z\.]+\.[a-z]+/
 
-if email_user.match(email_templ)
-    puts 'Дякую! Email правильний!'
-  else
-    puts 'Це не email'
-  end
+        if email_user.match(email_templ)
+            puts 'Дякую! Email правильний!'
+            break
+        else
+            puts 'Це не email'
+
+        end
+end 
