3d0bed927cf454c3b8833e90fe5788bc4b7b56d2
Ruby45 <discuss.ruby@gmail.com>
2023-08-15 11:52:45 +0300

    init commit

---
+++ apps/shop_media/lib/product.rb
@@ -0,0 +1,10 @@
+class Product
+  attr_reader :price, :qty
+
+  def initialize(price, qty)
+    @price = price, 
+    @qty = qty
+  end
+
+
+end
---
+++ apps/shop_media/main.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/product'
+require_relative 'lib/movie'
+require_relative 'lib/book'
+
+puts 'Ось такі товари у нас є: '
